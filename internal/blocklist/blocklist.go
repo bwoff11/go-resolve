33f2085d42415b356bb8bf6aadfc2ba3b7452a09
Brandon <brandon.wofford@unity3d.com>
2024-02-04 09:59:26 -0800

    better blocklist

--- internal/blocklist/blocklist.go
+++ internal/blocklist/blocklist.go
@@ -1,48 +1,95 @@
 package blocklist
 
 import (
-	"time"
+	"io"
+	"net/http"
+	"sync"
 
-	"github.com/bwoff11/go-resolve/internal/metrics"
 	"github.com/rs/zerolog/log"
+	"gopkg.in/yaml.v3"
 )
 
-// BlockList holds a list of blocked domains with reasons and categories.
-type BlockList struct {
-	Blocks []Block
-}
+// BlockList is a slice of Block, representing a list of blocked domains and their details.
+type BlockList []Block
 
-// Block holds information about a blocked domain.
+// Block represents a single blocked domain with its category and reason for being blocked.
 type Block struct {
-	Domain   string `yaml:"domain"`
-	Category string `yaml:"category"`
-	Reason   string `yaml:"reason"`
+	Domain   string `yaml:"domain"`   // Domain name to be blocked.
+	Category string `yaml:"category"` // Category of the reason for blocking (e.g., advertising).
+	Reason   string `yaml:"reason"`   // Long reason for the domain being blocked.
 }
 
-// New creates a new BlockList.
-func New(blocklists []string) *BlockList {
-	bl := &BlockList{}
+// New initializes a BlockList from a list of URLs pointing to blocklists.
+// It downloads and parses the blocklists concurrently for efficiency.
+func New(blockListURLs []string) *BlockList {
+	var combinedBlockList BlockList
+	var mutex sync.Mutex
+	var wg sync.WaitGroup
 
-	//bl.loadLocalBlockLists(cfg.Local)
-	//bl.loadRemoteBlockLists(cfg.Remote)
+	// Iterate over the provided URLs to download and parse their blocklists concurrently.
+	for _, url := range blockListURLs {
+		wg.Add(1)
+		go addBlocklist(url, &combinedBlockList, &mutex, &wg)
+	}
 
-	return bl
+	wg.Wait()
+	log.Info().Int("count", len(combinedBlockList)).Msg("Block list compiled")
+	return &combinedBlockList
 }
 
-// AddBlock adds a new block to the list.
-func (bl *BlockList) AddBlock(block Block) {
-	bl.Blocks = append(bl.Blocks, block)
+// addBlocklist handles the asynchronous download and parsing of a blocklist from a URL.
+// It appends the parsed blocklist to the combinedBlockList, ensuring thread-safe access.
+func addBlocklist(url string, combinedBlockList *BlockList, mutex *sync.Mutex, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	rawYAML, err := downloadBlockList(url)
+	if err != nil {
+		log.Error().Err(err).Str("url", url).Msg("Failed to download block list")
+		return
+	}
+
+	blockList, err := parseBlockList(rawYAML)
+	if err != nil {
+		log.Error().Err(err).Str("url", url).Msg("Failed to parse block list")
+		return
+	}
+
+	mutex.Lock()
+	*combinedBlockList = append(*combinedBlockList, *blockList...)
+	mutex.Unlock()
+
+	log.Info().Str("url", url).Msg("Block list downloaded and parsed")
 }
 
-// Query finds if a domain is in the blocklist and returns the corresponding block entry.
+// downloadBlockList downloads a block list from a given URL and returns its content as a byte slice.
+func downloadBlockList(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	return io.ReadAll(resp.Body)
+}
+
+// parseBlockList parses raw YAML content into a BlockList.
+func parseBlockList(rawYAML []byte) (*BlockList, error) {
+	var blockList BlockList
+	err := yaml.Unmarshal(rawYAML, &blockList)
+	return &blockList, err
+}
+
+// Query checks if a domain is present in the block list and returns the corresponding Block if found.
 func (bl *BlockList) Query(domain string) *Block {
-	startTime := time.Now()
-	for _, block := range bl.Blocks {
-		if block.Domain+"." == domain {
-			log.Debug().Str("domain", domain).Msg("Domain blocked")
+	for _, block := range *bl {
+		if block.Domain == domain {
 			return &block
 		}
 	}
-	metrics.BlocklistDuration.Observe(time.Since(startTime).Seconds())
 	return nil
+}
+
+// Check checks if a domain is present in the block list and returns true if found.
+func (bl *BlockList) Check(domain string) bool {
+	return bl.Query(domain) != nil
 }
